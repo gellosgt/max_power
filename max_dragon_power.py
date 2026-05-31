5ab733308777422be2218243a7d273dd5d8f8bc2
gellosgt <mr.gellos2014@gmail.com>
2026-05-31 19:14:59 +0300

    Update max_dragon_power.py

--- max_dragon_power.py
+++ max_dragon_power.py
@@ -1,24 +1,24 @@
-def max_dragon_power(N: int) -> int:
+def max_dragon_power(n: int) -> int:
     # Малые случаи
-    if N == 1:
+    if n == 1:
         return 1
-    if N == 2:
+    if n == 2:
         return 2
-    if N == 3:
+    if n == 3:
         return 3
-    if N == 4:
+    if n == 4:
         return 4  # 2*2 = 4
 
     # Общий случай N >= 5
-    r = N % 3
+    r = n % 3
     if r == 0:
-        return 3 ** (N // 3)
+        return 3 ** (n // 3)
     elif r == 1:
-        return 3 ** ((N - 4) // 3) * 4
+        return 3 ** ((n - 4) // 3) * 4
     else:  # r == 2
-        return 3 ** (N // 3) * 2
+        return 3 ** (n // 3) * 2
 
 # Основная программа
 if __name__ == "__main__":
-    N = int(input().strip())
-    print(max_dragon_power(N))
+    n = int(input().strip())
+    print(max_dragon_power(n))
